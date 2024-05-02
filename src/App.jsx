31c4e798b6460db1941e830511caa29e1a5423a2
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2024-05-02 15:35:29 +0200

    Updating Game Board with X

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./Component/GameBoard";
 import Player from "./Component/Player";
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
           <Player initialName="Player 1" symbol="X"></Player>
           <Player initialName="Player 2" symbol="0"></Player>
         </ol>
+        <GameBoard></GameBoard>
       </div>
+      <p>logs</p>
     </main>
   );
 }
---
+++ src/Component/GameBoard.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleGameBtn(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+  return (
+    <div>
+      <ol id="game-board">
+        {gameBoard.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                  <button onClick={() => handleGameBtn(rowIndex, colIndex)}>
+                    {playerSymbol}
+                  </button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
